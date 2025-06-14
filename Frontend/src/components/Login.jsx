59557e0b838cb9058fb8eb2b5a3009ef56c4559d
Rajjoshi77 <110289049+Rajjoshi77@users.noreply.github.com>
2025-06-14 17:21:30 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post("http://localhost:5001/api/auth/login", { email, password });
+            const res = await axios.post("https://memoryvault-enhanced.onrender.com/api/auth/login", { email, password });
             localStorage.setItem("token", res.data.token);
             alert("Login successful!");
             navigate("/");
